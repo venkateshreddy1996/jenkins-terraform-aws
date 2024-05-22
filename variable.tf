add29c1f024204e706e09cdf7bddcac7aadbbbbf
venkatesh <58203176+venkateshreddy1996@users.noreply.github.com>
2024-05-22 14:20:38 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "ecs_cluster_name" {
+  default = "my-ecs-cluster"
+}
+
+variable "desired_count" {
+  default = 2
+}
+
+variable "container_port" {
+  default = 80
+}
+
+variable "container_image" {
+  default = "nginx:latest"
+}
