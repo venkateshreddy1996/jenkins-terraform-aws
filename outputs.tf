b3735d347a794076467ae3d730480268374fa336
venkatesh <58203176+venkateshreddy1996@users.noreply.github.com>
2024-05-22 14:23:01 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "ecs_cluster_id" {
+  value = aws_ecs_cluster.main.id
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.main.name
+}
